fd32f6f1c01427a802682bca4d10cb27dc0af86d
yunkaixiao12 <yunkai.xiao@rcsd.ca>
2017-03-27 22:55:44 -0600

    new

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
   
     <div class="loginPage">
         
-        <form>
+        <form action = "index.php" , method = "get">
             
             Username/Email:
-            <input type="text" name="username" placeholder = "Enter your username."></br>
+            <input type="text" name="username" placeholder = "Enter your username.">
+            
+            </br>
             Password:
             <input type="password" placeholder = "Enter your password" name="password"></br>
             <input type="Submit" value="Login">
@@ -23,7 +25,17 @@
         </form>
         
     </div>
-    
+    <?php
+    if ($_GET ["username"] != null && $_GET ["password"] != null){
+        if ($_GET ["username"] == "admin" && $_GET ["password"] == "admin"){
+            header( 'Location: admin.php' );
+        }
+        if ($_GET ["username"] != "admin" || $_GET ["password"] != "admin")
+        {
+            echo "用户名或密码输入不正确。";
+        }
+    }
+        ?>
 </body>
 
 </html>
